10ddde74596192fb2ed606c09abf1e4afb6e58dd
hozukiMk3 <meta.maetel@gmail.com>
2019-03-19 16:05:05 +0900

    projectとサンプルを追加

---
+++ Dracaena/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dracaena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = @"C:\Working\Reference\glTF-Sample-Models\2.0\Box\glTF\";
+            var gltf = new glTFIO.glTFFile($"{dir}Box.gltf");
+            Console.Read();
+        }
+    }
+}
---
+++ glTFIO/glTFFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+
+namespace glTFIO
+{
+    public class glTFFile
+    {
+        public glTFFile(string path)
+        {
+            using (var stream = new System.IO.StreamReader(path))
+            {
+                var jsonObjects = JsonConvert.DeserializeObject(stream.ReadToEnd());
+                Console.WriteLine(jsonObjects);
+            }
+        }
+    }
+}
