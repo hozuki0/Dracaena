e08460aeeb9bcae5d0b54e4e7da49b74329543c0
hozukiMk3 <meta.maetel@gmail.com>
2019-03-20 16:16:37 +0900

    BufferLengthとOffsetの扱いが反対だったバグを解消

--- glTFIO/Internal/BufferViewChunk.cs
+++ glTFIO/Internal/BufferViewChunk.cs
@@ -9,8 +9,8 @@ namespace glTFIO.Internal
     public class BufferViewChunk : glTFDataChunkBase
     {
         public int Buffer { get; set; }
-        public int ByteOffset { get; set; } = 0;
-        public int? ByteLength { get; set; }
+        public int? ByteOffset { get; set; } = 0;
+        public int ByteLength { get; set; }
         public int? ByteStride { get; set; }
         public int? Target { get; set; }
         public string Name { get; set; }
